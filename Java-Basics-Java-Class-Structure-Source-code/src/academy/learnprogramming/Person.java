cf7b1092543f0a4fa366750bfd9640c7bca22984
isaccaldas <34041465+isaccaldas@users.noreply.github.com>
2022-01-03 12:46:57 -0300

    class structure

---
+++ Java-Basics-Java-Class-Structure-Source-code/src/academy/learnprogramming/Person.java
@@ -0,0 +1,19 @@
+package academy.learnprogramming;
+
+/**
+ * @author goran on 22/06/2017.
+ */
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+}
